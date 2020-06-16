2d224d7bddb31e6656c6089be1cca4193b5a0c58
Gakkykun <w-inagaki@ruri.waseda.jp>
2020-06-16 21:30:20 +0900

    Update an exercise file

--- excercise1.f90
+++ excercise1.f90
@@ -1,18 +1,18 @@
 program excercise1
     implicit none
-    real  :: DO_conc,T(degree_c)
+    real  :: DO_conc,T !Please avoid using parenthes in declaration. I deleted them.
     integer::i
      do i= 5,30,5
-       T(degree_c)=real(i)
-       DO_conc= Dissolved Oxygen(mg/l)
-        write(*,30)Dissolved oxygen 
+       T=real(i) ! Same as the above comment
+       DO_conc= Dissolved_Oxygen(T) ! Two words (i.e., Dissolved and oxygen) must be connected with such as '-' or '_'. Remove parenthes
+        write(*,30) DO_conc 
        30 format(F6.0)
      end do
      contains
-     real function calculate dissolved oxygen 
-     real, intent (in) :: T(degree_c)
-    dissolved oxygen(mg/l)=14.652-0.41022*T(degree_c)+0.00799*T^2-0.000077774*T^3
-    end function calculate dissolved oxygen
+     real function dissolved_oxygen(temperature) !This name must match with the name above (Dissolved_Oxygen) 
+     real, intent (in) :: T  ! Please avoid using parenthes. You can simply set T
+    dissolved_oxyge=14.652-0.41022*T+0.00799*T**2-0.000077774*T**3
+    end function dissolved_oxygen
 
 
 end program excercise1
